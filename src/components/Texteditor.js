30c9b887774d370a381acd82dbcd3430ff560d78
Amanp30~ <amanpareek300@gmail.com>
2023-05-14 18:22:43 +0530

    quill fixed color for dark theme

--- src/components/Texteditor.js
+++ src/components/Texteditor.js
@@ -2,34 +2,29 @@ import React from "react";
 import ReactQuill, { Quill } from "react-quill";
 import DOMPurify from "dompurify";
 import "react-quill/dist/quill.snow.css";
-import ImageResize from "quill-image-resize-module-react";
-import { ImageDrop } from "quill-image-drop-module";
-
-Quill.register("modules/imageDrop", ImageDrop);
-Quill.register("modules/imageResize", ImageResize);
 
 const modules = {
   toolbar: [
     //[{ 'font': [] }],
     [{ header: [1, 2, false] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
+    ["bold", "italic", "underline", "strike" /* , "blockquote" */],
     [
       { list: "ordered" },
       { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
+      //   { indent: "-1" },
+      //   { indent: "+1" },
     ],
-    ["link", "image"],
-    [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+    [/* "link", */ "image"],
+    [{ align: [] }, { color: [] } /* { background: [] } */], // dropdown with defaults from theme
     ["clean"],
   ],
-  imageDrop: {}, // Make sure to add this!!!
-  imageResize: {
-    parchment: Quill.import("parchment"),
-    displaySize: true,
-    // maxSize: [500, 500],
-    // height: 400, // add this line to set the height of the image resize module
-  },
+  //   imageDrop: {}, // Make sure to add this!!!
+  //   imageResize: {
+  //     parchment: Quill.import("parchment"),
+  //     displaySize: true,
+  //     // maxSize: [500, 500],
+  //     // height: 400, // add this line to set the height of the image resize module
+  //   },
 };
 
 const formats = [
