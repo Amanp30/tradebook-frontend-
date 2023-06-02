ad1feec85485e730134b1f75f84126353fc3edf4
Amanp30~ <amanpareek300@gmail.com>
2023-06-02 20:46:05 +0530

    best for upstox

--- src/helpers/brokerdata.js
+++ src/helpers/brokerdata.js
@@ -1,5 +1,5 @@
 export var brokerdata = {
-  upstox: { brokerage: 0.05, transactioncharges: "0.00345" },
+  upstox: { brokerage: 0.05, transactioncharges: "0.00325" },
   zerodha: { brokerage: 0.03, transactioncharges: "0.00345" },
   angelone: { brokerage: 0.25, transactioncharges: "0.00335" },
 };
